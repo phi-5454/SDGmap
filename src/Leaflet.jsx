0353e4b1f3dddfb3f50365258eb665d4ddce004c
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2024-03-23 10:31:52 +0000

    addPin

--- src/Leaflet.jsx
+++ src/Leaflet.jsx
@@ -1,18 +1,23 @@
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
-
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 
 function Leaflet() {
+    const position = [60.1699, 24.9384]
 
-    const position = [51.505, -0.09]
+    function addPin(coordinates) {
+        return (
+            <Marker position={coordinates}>
+            </Marker>
+        )
+    }
 
     return (
         <>
             <MapContainer center={position} zoom={20} style={{ height: "90vh" }}>
-            <TileLayer
-            attribution='Olli Glorioso'
-            url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=o28q90KHszO8WjJEWBy1"
-            />
+                <TileLayer
+                attribution='Olli Glorioso'
+                url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=o28q90KHszO8WjJEWBy1"
+                />
             </MapContainer>
         </>
     )
