600c09c0d12ccd302e15eedd184b731981777ba7
Younes <younes.elberkennou@aalto.fi>
2024-03-23 14:05:54 +0200

    Pin styling

--- src/Leaflet.jsx
+++ src/Leaflet.jsx
@@ -1,27 +1,36 @@
-import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { renderToStaticMarkup } from "react-dom/server";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import pinLibrary from "./pinInfo";
 
 function Leaflet({ pins, setPins }) {
-    const position = [60.1699, 24.9384]
+  console.log(pinLibrary);
+  const position = [60.1699, 24.9384];
+  const customIcon = L.divIcon({
+    className: "custom-icon",
+    html: renderToStaticMarkup(pinLibrary[0].icon),
+    iconSize: [30, 30], // Adjust the icon size as needed
+  });
 
-    function addPin(coordinates) {
-        return (
-            <Marker position={coordinates}>
-            </Marker>
-        )
-    }
+  function addPin(coordinates, icon) {
+    return <Marker position={coordinates} icon={icon}></Marker>;
+  }
 
-    return (
-        <>
-            <MapContainer center={position} zoom={20} style={{ height: "90vh" }}>
-                <TileLayer
-                attribution='Olli Glorioso'
-                url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=o28q90KHszO8WjJEWBy1"
-                />
-                {pins.map((pin) => addPin(pin.coordinates))}
-            </MapContainer>
-        </>
-    )
+  return (
+    <>
+      <MapContainer center={position} zoom={20} style={{ height: "90vh" }}>
+        <TileLayer
+          //attribution="Olli Glorioso"
+          url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=o28q90KHszO8WjJEWBy1"
+        />
+        {pins.map((pin) => addPin(pin.coordinates, customIcon))}
+      </MapContainer>
+    </>
+  );
 }
 
-export default Leaflet
+export default Leaflet;
+
--- src/pinInfo.jsx
+++ src/pinInfo.jsx
@@ -2,32 +2,56 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
 const Category = {
   Hazard: "Hazard",
+  Damage: "Damage",
+  Decay: "Decay",
+  Suggestion: "Suggestion",
   Other: "Other",
 };
 
-const Pin = {
-  StreetFlood: {},
-};
+const pinLibrary = [
+  {
+    key: 0,
+    name: "Flooded street",
+    category: Category.Damage,
+    icon: (
+      <div className="icon-container">
+        <div className="icon-text bg-red-600 border-white border-2 text-blue-200">
+          AAA
+        </div>
+        <FontAwesomeIcon icon={fas.faHouse} />
+      </div>
+    ),
+  },
+];
 
-const exPin = {
-  name: "SomePin",
-  category: Category.Other,
-  postition: [60.0, 60.0],
-  lifetime: 55000,
-};
-
-const exPinInst = {
-  pinType: exPin,
-  recordTime: 0,
-  freeInfo: "Some user-specified info",
-};
-
-const newPin = () => {
+const createPinData = () => {
   return {
     urgent: false,
+  };
+};
+
+// Factory method for pin objects.
+const pinObject = (props) => {
+  const Pin = {
+    StreetFlood: {},
+  };
+
+  const exPin = {
+    name: "SomePin",
+    category: Category.Other,
+    postition: [60.0, 60.0],
+    lifetime: 55000,
+  };
+
+  const exPinInst = {
+    pinType: exPin,
+    recordTime: 0,
+    freeInfo: "Some user-specified info",
   };
 };
 
@@ -35,4 +59,5 @@ const getPinInfo = () => {};
 
 const savePinInfo = (pin) => {};
 
-export default { getPinInfo, savePinInfo };
+//export default { getPinInfo, savePinInfo, pinLibrary };
+export default pinLibrary;
