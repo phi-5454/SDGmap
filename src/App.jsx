dcc015636a28e3c7f487761606c2aff754723f5f
Younes <younes.elberkennou@aalto.fi>
2024-03-23 12:53:21 +0200

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
 import "./App.css";
 import Leaflet from "./Leaflet";
+import { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
-const element = <FontAwesomeIcon icon={fas.faHouse} />;
-
 function App() {
+  const [pins, setPins] = useState([
+    {
+      coordinates: [60.1699, 24.9384],
+    },
+  ]);
+
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <Leaflet />
+      <Leaflet pins={pins} setPins={setPins} />
       <FontAwesomeIcon icon={fas.faHouse} />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- src/Leaflet.jsx
+++ src/Leaflet.jsx
@@ -1,18 +1,24 @@
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
-
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 
-function Leaflet() {
+function Leaflet({ pins, setPins }) {
+    const position = [60.1699, 24.9384]
 
-    const position = [51.505, -0.09]
+    function addPin(coordinates) {
+        return (
+            <Marker position={coordinates}>
+            </Marker>
+        )
+    }
 
     return (
         <>
             <MapContainer center={position} zoom={20} style={{ height: "90vh" }}>
-            <TileLayer
-            attribution='Olli Glorioso'
-            url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=o28q90KHszO8WjJEWBy1"
-            />
+                <TileLayer
+                attribution='Olli Glorioso'
+                url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=o28q90KHszO8WjJEWBy1"
+                />
+                {pins.map((pin) => addPin(pin.coordinates))}
             </MapContainer>
         </>
     )
