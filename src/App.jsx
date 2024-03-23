e6725ed4a752cd522abe5a19af5e4bfb2022c53a
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2024-03-23 15:23:15 +0000

    epic animation

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,51 @@ import { useSpring, animated } from 'react-spring';
 
 function App() {
   const [pins, setPins] = useState(pinsOnMap);
-  const props = useSpring({
+  const [showTitle, setShowTitle] = useState(true);
+
+  const titleProps = useSpring({
+    from: { opacity: 1 },
+    to: { opacity: 0 },
+    config: { duration: 1000 },
+    style: { color: 'red' },
+    onRest: () => setShowTitle(false),
+  });
+
+  function getMiddlePointOfWindow() {
+    return {
+      x: window.innerWidth / 3,
+      y: window.innerHeight / 3
+    };
+  }
+
+  titleProps.fontSize = '100px';
+  titleProps.backgroundColor = 'white';
+  titleProps.height = 0;
+  titleProps.width = 0;
+  titleProps.backgroundColor = 'white';
+  titleProps.position = 'absolute'; // Add this line
+  titleProps.top = (getMiddlePointOfWindow().y); // Add this line
+  titleProps.left = (getMiddlePointOfWindow().x); // Add this line
+  titleProps.transform = 'translate(-50%, -50%)'; // Add this line
+
+  const appProps = useSpring({
     to: { opacity: 1 },
     from: { opacity: 0 },
     delay: 1000
   })
 
   return (
-    <animated.div style={props}>
-      <>
-        <Leaflet pins={pins} setPins={setPins} class="leaflet-container" />
-        <CustomToolbar class='toolbar'/>
-      </>
-    </animated.div>
+    <>
+      <div>
+        <animated.h1  style={titleProps}>Cityworkshop</animated.h1>
+      </div>
+        <animated.div style={appProps}>
+          <>
+            <Leaflet pins={pins} setPins={setPins} class="leaflet-container" />
+            <CustomToolbar class='toolbar'/>
+          </>
+        </animated.div>
+    </>
   );
 }
 
