bf6a5c4e27ce74a13aabd2ba54b539c0cc727a90
Olli Glorioso <84347872+olliglorioso@users.noreply.github.com>
2024-03-23 15:00:22 +0000

    eeppinen animaatio

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import Leaflet from "./Leaflet";
 import { useState } from "react";
 import CustomToolbar from "./ToolBar";
 import { pinsOnMap } from "./pinInfo";
+import { useSpring, animated } from 'react-spring';
+
 
 function App() {
   const [pins, setPins] = useState(pinsOnMap);
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    delay: 1000
+  })
 
   return (
-    <>
-      <Leaflet pins={pins} setPins={setPins} class="leaflet-container" />
-      <CustomToolbar class='toolbar'/>
-    </>
+    <animated.div style={props}>
+      <>
+        <Leaflet pins={pins} setPins={setPins} class="leaflet-container" />
+        <CustomToolbar class='toolbar'/>
+      </>
+    </animated.div>
   );
 }
 
